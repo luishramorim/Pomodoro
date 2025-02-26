2e641e7c09c420c20b42b71c50e261bd83e3740e
Luis Amorim <luis@amorim.bio.br>
2025-02-26 13:04:55 -0300

    Initial Commit

---
+++ Pomodoro/PomodoroApp.swift
@@ -0,0 +1,20 @@
+//
+//  PomodoroApp.swift
+//  Pomodoro
+//
+//  Created by Luis Amorim on 26/02/25.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ PomodoroTests/PomodoroTests.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroTests.swift
+//  PomodoroTests
+//
+//  Created by Luis Amorim on 26/02/25.
+//
+
+import Testing
+@testable import Pomodoro
+
+struct PomodoroTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
